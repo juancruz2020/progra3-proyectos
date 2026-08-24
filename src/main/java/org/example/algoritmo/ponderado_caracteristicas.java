7819b9d5384d1126d0419d2a603655dd4a59d849
sinesiuade <dsinesi@uade.edu.ar>
2026-08-24 16:31:29 -0300

    Update ponderado_caracteristicas.java

--- src/main/java/org/example/algoritmo/ponderado_caracteristicas.java
+++ src/main/java/org/example/algoritmo/ponderado_caracteristicas.java
@@ -2,20 +2,46 @@ package org.example.algoritmo;
 
 import org.example.objetos.personaje;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ponderado_caracteristicas {
 
-    int ponderado;
+    private Map<String, Integer> ponderaciones = new HashMap<>();
 
-    public int que_pregunto(List<personaje> personajes){
-        for (int i = 0; i < personajes.size(); i++){
-            personaje personaje = personajes.get(i);
-            if(personaje.isGenero()){
-                ponderado++;
+    private void init_map() {
+        ponderaciones.put("genero", 0);
+        ponderaciones.put("anteojos", 0);
+        ponderaciones.put("sombrero", 0);
+        ponderaciones.put("barba", 0);
+        ponderaciones.put("sonrisa", 0);
+        ponderaciones.put("pelo_largo", 0);
+    }
+
+    public Map<String, Integer> que_pregunto(List<personaje> personajes) {
+        init_map();
+        for (personaje personaje : personajes) {
+            if (personaje.isGenero()) {
+                ponderaciones.put("genero", ponderaciones.get("genero") + 1);
+            }
+            if (personaje.isAnteojos()) {
+                ponderaciones.put("anteojos", ponderaciones.get("anteojos") + 1);
+            }
+            if (personaje.isSombrero()) {
+                ponderaciones.put("sombrero", ponderaciones.get("sombrero") + 1);
+            }
+            if (personaje.isBarba()) {
+                ponderaciones.put("barba", ponderaciones.get("barba") + 1);
+            }
+            if (personaje.isSonrisa()) {
+                ponderaciones.put("sonrisa", ponderaciones.get("sonrisa") + 1);
+            }
+            if (personaje.isPelo_largo()) {
+                ponderaciones.put("pelo_largo", ponderaciones.get("pelo_largo") + 1);
             }
         }
 
+        return this.ponderaciones;
     }
-
 }
